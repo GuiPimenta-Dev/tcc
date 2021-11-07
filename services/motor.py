a2f8f330c314eadabf8fda0a7df916d6a175a787
GUILHERME PIMENTA <guilherme.pimentahays@modec.com>
2021-11-07 13:52:57 -0300

    removing __repr__ statement from motor

--- services/motor.py
+++ services/motor.py
@@ -6,14 +6,6 @@ class MotorService(MotorBusiness):
         self.settings = self.load = self.voltage = self.power_factor = self.create_motor(params)
         self.settings_coords = self.get_coords(self.settings)
 
-    def __repr__(self):
-        motor_informations = f'settings: {self.settings}\n'
-        motor_informations += f'load: {self.load}\n'
-        motor_informations += f'voltage: {self.voltage}\n'
-        motor_informations += f'fp: {self.power_factor}\n'
-
-        return motor_informations
-
     def update_load(self, load: float):
         self.load['settings']['load'] = load * 0.746 + self.load['settings']['losses']
         return self.load_update(params=self.load)
