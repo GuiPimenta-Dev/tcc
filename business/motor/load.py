21171b16c9acda882da43ffa2cf1b27f2cd411da
GUILHERME ALVES PIMENTA <guialvespimenta27@gmail.com>
2022-01-26 19:54:45 -0300

    remvoing max load update

--- business/motor/load.py
+++ business/motor/load.py
@@ -12,12 +12,6 @@ class Load(MotorBaseBusiness):
 
         return self._get_scaled_coords(model=model)
 
-    def _max_load_update(self, model: MotorModel):
-        self.__calculate_new_delta(model=model)
-        self.__update_polar_params(model=model)
-        self._update_rectangular_params(model=model)
-        return model.rectangular
-
     def __calculate_new_delta(self, model: MotorModel):
         phase = (model.hp_load * abs(model.Xs) * 1000) / (3 * model.Vt * model.Ea)
         model.delta = self.degree(self.rad(-1 * self.degree(asin(phase))))
