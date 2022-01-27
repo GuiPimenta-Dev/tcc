7c99977886634b5306faa02589da71ebce46f9aa
GUILHERME ALVES PIMENTA <guialvespimenta27@gmail.com>
2022-01-27 15:46:50 -0300

    fixing test

--- tests/test_connected_generator.py
+++ tests/test_connected_generator.py
@@ -19,26 +19,12 @@ service = ConnectedGeneratorService(model=GeneratorModel(**args))
 class TestConnectedGenerator:
     def test_motor_settings(self):
         expected_result = {
-            "coords": {
-                "Vt": (32.26, 0.0),
-                "Ia": (6.45, -4.838709677419354),
-                "Ea": (45.0, 15.64516129032258),
-                "RaIa": (0.65, -0.48387096774193544),
-                "jXsIa": (12.1, 16.129032258064516),
-            },
-            "labels": {
-                "Vt": "480 ∠ 0°",
-                "Ia": "120 ∠ -36.87°",
-                "Ea": "708.91 ∠ 19.17°",
-                "RaIa": "12.0 ∠ -36.87°",
-                "jXsIa": "300.0 ∠ 53.13°",
-            },
-            "sliders": {
-                "load": {"min": 0, "max": 180.0, "value": 120},
-                "voltage": {"min": 232, "max": 720.0, "value": 708.91},
-                "power_factor": {"min": 0, "max": 1, "value": 0.8},
-            },
-        }
+            'coords': {'Vt': (32.26, 0.0), 'Ia': (6.45, -4.838709677419354), 'Ea': (45.0, 15.64516129032258),
+                       'RaIa': (0.65, -0.48387096774193544), 'jXsIa': (12.1, 16.129032258064516)},
+            'labels': {'Vt': '480 ∠ 0°', 'Ia': '120 ∠ -36.87°', 'Ea': '708.91 ∠ 19.17°', 'RaIa': '12.0 ∠ -36.87°',
+                       'jXsIa': '300.0 ∠ 53.13°'}, 'sliders': {'load': {'min': 0, 'max': 180.0, 'value': 85.42},
+                                                               'voltage': {'min': 232, 'max': 720.0, 'value': 708.91},
+                                                               'power_factor': {'min': 0, 'max': 1, 'value': 0.8}}}
         result = service.settings_coords
         assert result == expected_result
 
